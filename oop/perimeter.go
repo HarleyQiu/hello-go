8a8efe9b2792ea051ec03867b7fb77c5c26ff633
QiuLin-hao <210554114@qq.com>
2024-05-11 17:14:52 +0800

    重构结构体和方法

--- oop/perimeter.go
+++ oop/perimeter.go
@@ -6,13 +6,16 @@ type Rectangle struct {
 	Width  float64
 	Height float64
 }
-
 type Circle struct {
 	Radius float64
 }
 
 func Perimeter(rectangle Rectangle) float64 {
 	return 2 * (rectangle.Width + rectangle.Height)
+}
+
+type Shape interface {
+	Area() float64
 }
 
 func Area(rectangle Rectangle) float64 {
