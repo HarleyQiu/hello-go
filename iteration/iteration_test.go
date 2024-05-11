e4ddf859ba2559b782edaca64a6920a298dd7862
QiuLin-hao <210554114@qq.com>
2024-05-11 08:25:58 +0800

    现在迭代可以指定迭代次数同时增加了一堆测试案例

--- iteration/iteration_test.go
+++ iteration/iteration_test.go
@@ -3,9 +3,30 @@ package iteration
 import "testing"
 
 func TestRepeat(t *testing.T) {
-	repeatd := Repeat("a")
-	expected := "aaaaa"
-	if repeatd != expected {
-		t.Errorf("expected %q repeat %q", expected, repeatd)
+
+	assertCorrectRepetition := func(t *testing.T, expected, repeatd string) {
+		t.Helper()
+		if repeatd != expected {
+			t.Errorf("expected %q repeat %q", expected, repeatd)
+		}
 	}
+
+	t.Run("repeat character 'a' five times", func(t *testing.T) {
+		repeatd := Repeat("a", 5)
+		expected := "aaaaa"
+		assertCorrectRepetition(t, expected, repeatd)
+	})
+
+	t.Run("repeat character 'b' three times", func(t *testing.T) {
+		repeatd := Repeat("b", 3)
+		expected := "bbb"
+		assertCorrectRepetition(t, expected, repeatd)
+	})
+
+	t.Run("repeat character 'x' zero times", func(t *testing.T) {
+		repeatd := Repeat("x", 0)
+		expected := ""
+		assertCorrectRepetition(t, expected, repeatd)
+	})
+
 }
--- iteration/repeat.go
+++ iteration/repeat.go
@@ -1,8 +1,9 @@
 package iteration
 
-func Repeat(character string) string {
+// Repeat 返回一个新字符串，由指定的字符重复指定次数组成。
+func Repeat(character string, count int) string {
 	var repeatd string
-	for i := 0; i < 5; i++ {
+	for i := 0; i < count; i++ {
 		repeatd = repeatd + character
 	}
 	return repeatd
