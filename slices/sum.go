34cc297478798fc9d2ec0277be990615740be052
QiuLin-hao <210554114@qq.com>
2024-05-11 13:02:11 +0800

    避免因传入空数组而报错的问题 重构项目位置

--- slices/sum.go
+++ slices/sum.go
@@ -1,4 +1,4 @@
-package main
+package slices
 
 // Sum 计算一个整数切片（slice）中所有元素的和。
 // 它接受一个整数切片 `numbers` 并返回它们的总和。
@@ -12,12 +12,23 @@ func Sum(numbers []int) int {
 
 // SumAll 接受多个整数切片，计算每个切片的和，然后返回一个包含这些和的切片。
 // `numbersToSum` 是一个可变参数，允许传入任意数量的整数切片。
-func SumAll(numbersToSum ...[]int) (sums []int) {
-	lengthOfNumbers := len(numbersToSum) // 获取传入切片的数量。
-	sums = make([]int, lengthOfNumbers)  // 根据切片数量创建一个新的整数切片，用于存储每个切片的和。
-
-	for i, numbers := range numbersToSum { // 遍历每个切片。
-		sums[i] = Sum(numbers) // 调用 Sum 函数计算当前切片的和，并将结果存储到 sums 切片中。
+func SumAll(numbersToSum ...[]int) []int {
+	var sums []int
+	for _, numbers := range numbersToSum {
+		sums = append(sums, Sum(numbers))
 	}
-	return sums // 返回包含所有和的切片。
+	return sums
+}
+
+func SumAllTails(numbersToSum ...[]int) []int {
+	var sums []int
+	for _, numbers := range numbersToSum {
+		if len(numbers) == 0 {
+			sums = append(sums, 0)
+		} else {
+			tail := numbers[1:]
+			sums = append(sums, Sum(tail))
+		}
+	}
+	return sums
 }
