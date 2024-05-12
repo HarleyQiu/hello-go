b2da8e3fca1e117ab407edc730817edc42698879
QiuLin-hao <210554114@qq.com>
2024-05-12 10:32:43 +0800

    实现针对Maps的增删改查

--- dictionary/dictionary.go
+++ dictionary/dictionary.go
@@ -1,12 +1,18 @@
 package dictionary
 
-import (
-	"errors"
-)
-
 type Dictionary map[string]string
 
-var ErrNotFound = errors.New("找不到您要找的词")
+var (
+	ErrNotFound         = DictionaryErr("找不到您要找的词")
+	ErrWordExists       = DictionaryErr("无法添加单词，因为它已经存在")
+	ErrWordDoesNotExist = DictionaryErr("无法更新单词，因为它不存在")
+)
+
+type DictionaryErr string
+
+func (e DictionaryErr) Error() string {
+	return string(e)
+}
 
 func (d Dictionary) Search(word string) (string, error) {
 	definition, ok := d[word]
@@ -14,4 +20,36 @@ func (d Dictionary) Search(word string) (string, error) {
 		return "", ErrNotFound
 	}
 	return definition, nil
+}
+
+func (d Dictionary) Add(word, definition string) error {
+	_, err := d.Search(word)
+
+	switch err {
+	case ErrNotFound:
+		d[word] = definition
+	case nil:
+		return ErrWordExists
+	default:
+		return err
+	}
+
+	return nil
+}
+
+func (d Dictionary) Update(word, definition string) error {
+	_, err := d.Search(word)
+	switch err {
+	case ErrNotFound:
+		return ErrWordDoesNotExist
+	case nil:
+		d[word] = definition
+	default:
+		return err
+	}
+	return nil
+}
+
+func (d Dictionary) Delete(word string) {
+	delete(d, word)
 }
